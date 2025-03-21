d5dc48cc6b7265d1c852c2516793c6c73d261bc7
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-21 21:15:59 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,55 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray()
+	{
+		int[] nums = new int[5];
+
+		for (int i = 0; i<5; i++)
+		{
+			nums[i]=(i+1);
+		}
+		return nums;
+	}
+
+	public static String[] createArray(String str1, String str2, String str3, String str4)
+	{
+		String[] strArray = {str1, str2, str3, str4};
+		return strArray;
+	}
+
+	public static int findValue(int num, int[] list)
+	{
+		int dig = 0;
+		for (int i = 0; i<list.length; i++)
+		{
+			if (list[i]==num)
+			{
+				return dig;
+			}
+			dig++;
+		}
+
+		return -1;
+	}
+
+
+	public static int findThirdValue(String line, String[] list)
+	{
+		int occurence=0;
+		for (int i=0; i<list.length; i++)
+		{
+			if (list[i]==line)
+			{
+				occurence++;
+			}
+			if (occurence==3)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
 
 }
